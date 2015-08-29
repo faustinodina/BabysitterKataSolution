15e7d2dcd700cb62c6e1e3684415f693e7679d9d
Faustino Dina <faustino.dina@gmail.com>
2015-08-29 13:08:38 -0400

    First BabysitterKata test

---
+++ BabysitterKata/BabysitterWageCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabysitterKata
+{
+    public class BabysitterWageCalculator
+    {
+        private const int MIN_ALLOWED_START_TIME = 17 * 60; // 5pm, in minutes since beginning of the day
+
+        public decimal calculate(DateTime startTime)
+        {
+            DateTime minCheckIn = startTime.Date.AddMinutes(MIN_ALLOWED_START_TIME);
+            if (startTime < minCheckIn)
+                throw new ArgumentOutOfRangeException();
+
+            return 0;
+        }
+    }
+}
---
+++ BabysitterKataTest/AllTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabysitterKata;
+
+namespace BabysitterKataTest
+{
+    [TestClass]
+    public class AllTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Checked in before 5pm", AllowDerivedTypes=true )]
+        public void DoNotAllowBabysitterChecksInBeforeMinAllowedTime() 
+        {
+            BabysitterWageCalculator calculator = new BabysitterWageCalculator();
+            Decimal salary = salary = calculator.calculate(new DateTime(2015, 1, 1, 16, 59, 59));
+        }
+    }
+}
