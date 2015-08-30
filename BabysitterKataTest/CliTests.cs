286e0126dc338d832fab40bc9c29ebe3de8234af
Faustino Dina <faustino.dina@gmail.com>
2015-08-30 12:28:42 -0400

    Cli handles incorrect number of arguments

--- BabysitterKataTest/CliTests.cs
+++ BabysitterKataTest/CliTests.cs
@@ -15,5 +15,13 @@ namespace BabysitterKataTest
             string[] args = { "8/29/2015 22:30", "8/29/2015 23:30", "8/30/2015 1:30" };
             Assert.AreEqual("Your salary is 44", cliHelper.execute(args));
         }
+
+        [TestMethod]
+        public void CliHandlesIncorrectNumberOfArguments()
+        {
+            BabysitterKataCli cliHelper = new BabysitterKataCli();
+            string[] args = { "8/29/2015 22:30", "8/29/2015 23:30" };
+            Assert.AreEqual("Bad arguments", cliHelper.execute(args).Substring(0, 13));
+        }
     }
 }
